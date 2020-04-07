0100239ec9c223a7b7eb36e4b38f63ec107ed946
TistaTheWolf <chris.batista@ruralsource.com>
2020-04-07 10:09:11 -0600

    Routing configuration setup and demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/Homepage/homepage.component.jsx'
+
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div> 
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
