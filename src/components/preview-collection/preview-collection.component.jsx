e8050798a9ec58cbd87e43eb55ef89953fde4f4b
TistaTheWolf <chris.batista@ruralsource.com>
2020-04-09 08:37:13 -0600

    FMinished shop page and collection preview

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from '../collection-item/collection-item.component'
 import "./preview-collection.styles.scss";
 
 const PreviewCollection = ({ title, items }) => (
@@ -7,8 +8,8 @@ const PreviewCollection = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({id, ...itemProps}) => (
+          <CollectionItem key={id} {...itemProps}/>
         ))}
     </div>
   </div>
